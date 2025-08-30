d2de33d69ad596bbf575bb695a1ba07bc3ba9fe5
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-08-30 01:07:04 -0300

    feature: header

---
+++ src/app/ui/header/Header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from 'next/link';
+export default function Header() {
+
+
+    return (
+
+
+        <header>
+
+            <div>
+                <Image
+                    src="/header/logo.svg"
+                    alt="Logo TheBox"
+                    width={38}
+                    height={48}
+                />
+                <Link href="/" aria-label="TheBox Home">
+                    TheBox
+                </Link>
+
+            </div>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="#about" aria-label="About section"> About</Link>
+                    </li>
+                    <li>
+                        <Link href="#services" aria-label="Services section"> Services</Link>
+                    </li>
+                    <li>
+                        <Link href="#contact" aria-label="Contact section"> Contact</Link>
+                    </li>
+                </ul>
+                
+            </nav>
+        </header>
+    )
+
+
+}
