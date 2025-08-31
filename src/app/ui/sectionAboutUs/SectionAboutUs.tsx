a04aafb8d109b9deb1e620a53a02bff6e976a8b0
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-08-31 20:23:58 -0300

    style(home): section about us

--- src/app/ui/sectionAboutUs/SectionAboutUs.tsx
+++ src/app/ui/sectionAboutUs/SectionAboutUs.tsx
@@ -1,28 +1,23 @@
 import Image from "next/image";
 import Link from 'next/link';
+import styles from './SectionAboutUs.module.scss';
 
 
 export function SectionAboutUs() {
-
-
-
     return (
+        <div className={styles.sectionAboutUsWrapper}><Image
+            src={"/home/aboutUsBanner.svg"}
+            alt="About us"
+            width={902}
+            height={646}
+            className={styles.aboutUsImage}
+        />
 
+            <div className={styles.aboutUsContent}>
 
-        <div>
-
-            <Image
-                src={"/home/aboutUsBanner.svg"}
-                alt="About us"
-                width={902}
-                height={646}
-            />
-
-            <div>
-
-                <h3>About us</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum doloremque autem suscipit minus, at exercitationem numquam laudantium expedita veritatis adipisci magnam cumque quisquam error inventore ipsum officia quam atque veniam.</p>
-                <Link href="/about-us"> More on Our History</Link>
+                <h3 className={styles.aboutUsTitle}    >About us</h3>
+                <p className={styles.aboutUsDescription}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum doloremque autem suscipit minus, at exercitationem numquam laudantium expedita veritatis adipisci magnam cumque quisquam error inventore ipsum officia quam atque veniam.</p>
+                <Link href="/about-us" className={styles.aboutUsLink}> More on Our History</Link>
             </div>
         </div>
     )
