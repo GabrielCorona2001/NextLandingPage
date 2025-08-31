90422a8497839ab1811b7643779cf64e205c3490
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-08-30 23:05:42 -0300

    chore(mainBanner): create placeholder data

---
+++ src/app/lib/placeholderData.ts
@@ -0,0 +1,25 @@
+const projects = [
+
+    {
+        name: "Project 1",
+        image: "https://placehold.co/1440x646?text=Project+1",
+    },
+    {
+        name: "Project 2",
+        image: "https://placehold.co/1440x646?text=Project+2"
+    },
+    {
+        name: "Project 3",
+        image: "https://placehold.co/1440x646?text=Project+3"
+    },
+    {
+        name: "Project 4",
+        image: "https://placehold.co/1440x646?text=Project+4"
+    },
+    {
+        name: "Project 5",
+        image: "https://placehold.co/1440x646?text=Project+5"
+    },
+
+
+]
