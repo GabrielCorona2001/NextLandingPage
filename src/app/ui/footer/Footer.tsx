21c7d555b118e6d868a981d8895c7fb3547a3d44
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-09-01 22:54:12 -0300

    feat(footer): footer text and style

---
+++ src/app/ui/footer/Footer.tsx
@@ -0,0 +1,6 @@
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}> TheBox Company © 2022. All Rights Reserved </footer>)
+}
