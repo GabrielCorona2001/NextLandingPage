61334e514d39ffd51999a3f6477017fd78dd83ab
Gabriel Corona <gabrielcoronanunes2001@gmail.com>
2025-09-01 21:08:32 -0300

    feat(form): create form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import { Main } from "next/document";
 import MainBanner from "./ui/mainBanner/MainBanner";
 import { CardsWrapper } from "./ui/cards/CardsWrapper";
 import { SectionAboutUs } from "./ui/sectionAboutUs/SectionAboutUs";
+import { Form } from "./ui/form/Form";
 
 export default function Home() {
   return (
 
     <>
-    <MainBanner />
-    <CardsWrapper />
-    <SectionAboutUs />
-
+      <MainBanner />
+      <CardsWrapper />
+      <SectionAboutUs />
+      <Form />
     </>
   );
 }
